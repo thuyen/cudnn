45bdbe419349046d4fd3347695808a04fdcad3b9
Thuyen Ngo <vanthuyen@gmail.com>
2018-07-05 12:58:05 -0700

    init batch_norm

--- generic/BatchNorm.cpp
+++ generic/BatchNorm.cpp
@@ -3,30 +3,6 @@
 #include <ATen/Config.h>
 #include <ATen/cuda/CUDAConfig.h>
 
-#if !AT_CUDNN_ENABLED()
-
-namespace at { namespace native {
-
-// See Note [ATen preprocessor philosophy]
-
-std::tuple<Tensor, Tensor, Tensor> cudnn_batch_norm(
-    const Tensor& input, const Tensor& weight,
-    const Tensor& bias, const Tensor& running_mean, const Tensor& running_var,
-    bool training, double exponential_average_factor, double epsilon) {
-  throw std::runtime_error("cudnn_batch_norm: ATen not compiled with cuDNN support");
-}
-
-std::tuple<Tensor, Tensor, Tensor> cudnn_batch_norm_backward(
-    const Tensor& input, const Tensor& grad_output, const Tensor& weight,
-    const Tensor& running_mean, const Tensor& running_var,
-    const Tensor& save_mean, const Tensor& save_var,
-    double epsilon) {
-  throw std::runtime_error("cudnn_batch_norm_backward: ATen not compiled with cuDNN support");
-}
-
-}}  // namespace at::native
-
-#else // AT_CUDNN_ENABLED
 
 #include <ATen/cudnn/Descriptors.h>
 #include <ATen/cudnn/Types.h>
@@ -53,34 +29,8 @@ std::tuple<Tensor, Tensor, Tensor> cudnn_batch_norm(
     const Tensor& bias_t, const Tensor& running_mean_t, const Tensor& running_var_t,
     bool training, double exponential_average_factor, double epsilon)
 {
-  TensorArg input{ input_t, "input", 1 },
-            weight{ weight_t, "weight", 2 },
-            bias{ bias_t, "bias", 3 },
-            running_mean{ running_mean_t, "running_mean", 4 },
-            running_var{ running_var_t, "running_var", 5 };
-  CheckedFrom c = "cudnn_batch_norm";
+  // axis
   setCuDNNStreamToCurrent();
-
-  checkAllDefined(c, {input, weight, bias});
-  if (!training) {
-    checkAllDefined(c, {running_mean, running_var});
-  }
-  checkAllSameGPU(c, {input, weight, bias, running_mean, running_var});
-  if (input->type().scalarType() == ScalarType::Half) {
-    checkScalarType(c, weight, ScalarType::Float);
-  } else {
-    checkAllSameType(c, {input, weight});
-  }
-  checkAllSameType(c, {weight, bias, running_mean, running_var});
-  // TODO: is weight required to be contiguous?
-  checkAllContiguous(c, {input, weight, bias, running_mean, running_var});
-  checkDimRange(c, input, 2, 6 /* exclusive */);
-  auto num_features = input->size(1);
-  for (auto t : {weight, bias, running_mean, running_var}) {
-    if (t->defined()) {
-      checkNumel(c, t, num_features);
-    }
-  }
 
   cudnnBatchNormMode_t mode;
   if (input->dim() == 2) {
@@ -98,8 +48,9 @@ std::tuple<Tensor, Tensor, Tensor> cudnn_batch_norm(
 
   auto handle = getCudnnHandle();
   auto dataType = getCudnnDataType(*input);
-  TensorDescriptor idesc{ *input, 4 };  // input descriptor
-  TensorDescriptor wdesc{ expandScale(*weight, input->dim()), 4 };  // descriptor for weight, bias, running_mean, etc.
+
+  TensorDescriptor idesc{ishape, istride};  // input descriptor // NCX1
+  TensorDescriptor wdesc{wshape, wstride};  // descriptor for weight, bias, running_mean, etc. // 1C11
 
   Constant one(dataType, 1);
   Constant zero(dataType, 0);
@@ -152,31 +103,8 @@ std::tuple<Tensor, Tensor, Tensor> cudnn_batch_norm_backward(
     const Tensor& save_mean_t, const Tensor& save_var_t,
     double epsilon)
 {
-  TensorArg input{ input_t, "input", 1 },
-            grad_output{ grad_output_t, "grad_output", 2 },
-            weight{ weight_t, "weight", 3 },
-            save_mean{ save_mean_t, "save_mean", 4 },
-            save_var{ save_var_t, "save_var", 5 };
-  CheckedFrom c = "cudnn_batch_norm_backward";
+  // axis
   setCuDNNStreamToCurrent();
-
-  checkAllDefined(c, {input, grad_output, weight, save_mean, save_var});
-  checkAllSameGPU(c, {input, grad_output, weight, save_mean, save_var});
-  if (input->type().scalarType() == ScalarType::Half) {
-    checkScalarType(c, weight, ScalarType::Float);
-  } else {
-    checkAllSameType(c, {input, weight});
-  }
-  checkAllSameType(c, {input, grad_output});
-  checkAllSameType(c, {weight, save_mean, save_var});
-  // TODO: is weight required to be contiguous?
-  checkAllContiguous(c, {input, grad_output, save_mean, save_var});
-  checkDimRange(c, input, 2, 6 /* exclusive */);
-  checkSameSize(c, input, grad_output);
-  auto num_features = input->size(1);
-  for (auto t : {weight, save_mean, save_var}) {
-    checkNumel(c, t, num_features);
-  }
 
   cudnnBatchNormMode_t mode;
   if (input->dim() == 2) {
@@ -196,8 +124,8 @@ std::tuple<Tensor, Tensor, Tensor> cudnn_batch_norm_backward(
   auto handle = getCudnnHandle();
   auto dataType = getCudnnDataType(*input);
 
-  TensorDescriptor idesc{ *input, 4 };  // input, output, grad_output descriptor
-  TensorDescriptor wdesc{ expandScale(*weight, input->dim()), 4 };  // descriptor for weight, bias, save_mean, etc.
+  TensorDescriptor idesc{ishape, istride, 4 };  // input, output, grad_output descriptor
+  TensorDescriptor wdesc{wshape, wstride, 4 };  // descriptor for weight, bias, save_mean, etc.
 
   Constant one(dataType, 1);
   Constant zero(dataType, 0);
@@ -219,4 +147,3 @@ std::tuple<Tensor, Tensor, Tensor> cudnn_batch_norm_backward(
 
 }}  // namespace native
 
-#endif
